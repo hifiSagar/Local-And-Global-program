2e154be216ec35aa9d5ee9f527c83145ef58469e
SAGAR T <ssagu6661@gmail.com>
2024-08-30 09:52:30 -0400

    Add files via upload

---
+++ c31.c
@@ -0,0 +1,35 @@
+/*LOCAL VARIABLES,,, AND  GLOBAL VARIABLES*/
+
+
+/*LOCAL VARIABLE*/
+// #include<stdio.h>
+// int add(){
+//     int a= 10,b=20;
+//     return a+b;
+
+// }
+
+// int main(){
+// int var = 10;
+// printf("%d \n",var);
+// printf("%d \n",add());
+//     return 0;
+// }
+
+/*GLOBAL VARIABLE*/
+#include<stdio.h>
+int a=100;
+int add(){
+    int a=20,b=20;
+    printf("%d\n",a);
+    return a+b;
+
+}
+int main(){
+ int var=10;
+ add();
+ printf("%d \n",var);
+printf("%d \n",a);
+
+return 0;
+}
